de19ce8a3655e001fbce3fb811354df1edbf6290
alexanderagreda <alexanderagreda@gmail.com>
2019-09-01 20:57:14 -0500

    insert line break

--- 1.primerCodigo/app.js
+++ 1.primerCodigo/app.js
@@ -1,6 +1,6 @@
 //declaration of variables
-let nombre = prompt('¿Cuál es tu nombre?');
-let edad = prompt('¿Cuál es tu edad?');
+let nombre = prompt('Preguntas: \n\n¿Cuál es tu nombre?');
+let edad = prompt('Preguntas \n\n¿Cuál es tu edad?');
 
 //write in html element
 document.getElementById('app').innerHTML = `Bienvenido ${nombre} ${edad} años`;
